584e614ceca8e5e87811c8bc26979ae82db908de
unknown <piffardb@gmail.com>
2022-07-11 17:28:51 -0700

    Adding all csv files for deliverable 1. That is retirement_titles which is a list of employees from the specified retriement age with the title listed, unique_titles which is the same list except only the entry with the most recent title is listed, and only current employees are listed, and retiring_titles which is a count of retiring employees by job title. The sql code used to get these tables is included in the Employee_Database_challenge.sql file under the Queries sub-folder. All the csv files are under the Data subfolder

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -96,3 +96,16 @@ FROM dept_manager AS dm
         ON (dm.dept_no = d.dept_no)
     INNER JOIN current_emp AS ce
         ON (dm.emp_no = ce.emp_no);
+		
+-- DEPT Retirees: add department infor to current employyee list
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name,
+	de.to_date
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
